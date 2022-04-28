fd5d4fa614b1e7545db8b5220848f6b82412ab39
David Corrales <jdcorrales27@gmail.com>
2022-04-28 17:27:02 -0500

    Update numbers range and instructions added ;)

--- evenornot.py
+++ evenornot.py
@@ -1,19 +1,24 @@
 #Even or Uneven number
+print("---Welcome to Even or Uneven numbers---")
+print("-----Write Q or anything else to exit------")
 def evenOrEven():
-    print("---Welcome to Even or Uneven numbers---")
-    print("---Write anything else to exit---")
     try:
-        number = int(input("What number are you thinking? >> "))
-        while True:
-            if number%2 == 0:
-                print("It's an even number!, Can you add another number? ")
-                number = int(input(">> "))
-            else:
-                print("It's an uneven number!, Can you add another number?")
-                number = int(input(">> ")) 
+        number = int(input("What number are you thinking? between 1 and 1.000 >> "))
+        if number >= 1 and number <= 1000:    
+            while True:
+                if number%2 == 0:
+                    print("It's an even number!, Can you add another number? ")
+                    number = int(input(">> "))
+                else:
+                    print("It's an uneven number!, Can you add another number?")
+                    number = int(input(">> ")) 
+        else:
+            print("****Please follow the instructions****")
+            evenOrEven()
     except:
         return print("GoodBye!")
 evenOrEven()
+
 #date: 28 - 4- 22
 #Thank you
 #Adding git branch
