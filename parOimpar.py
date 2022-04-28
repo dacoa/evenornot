02e53c217afd22de994872b79b4b4595a54ddcf4
David Corrales <jdcorrales27@gmail.com>
2022-04-28 13:50:09 -0500

    First proyect from FreeCodeCamp

---
+++ parOimpar.py
@@ -0,0 +1,20 @@
+#Even or Uneven number
+def evenOrEven():
+    
+    print("---Welcome to Even or Uneven numbers---")
+    print("---Write anything else to exit---")
+    try:
+        number = int(input("What number are you thinking? >> "))
+        while True:
+            if number%2 == 0:
+                print("It's an even number!, Can you add another number? ")
+                number = int(input(">> "))
+            else:
+                print("It's an uneven number!, Can you add another number?")
+                number = int(input(">> "))
+    except:
+        return print("GoodBye!")
+evenOrEven()
+#Primer programa para subir a github 
+
+#print("Hola, hay que esperar")
